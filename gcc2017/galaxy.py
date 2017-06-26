8da2fbf204a13864061a12b975b8eb89a90f161d
Hervé  MENAGER <herve.menager@pasteur.fr>
2017-06-26 20:51:27 +0200

    improve galaxy export module

--- gcc2017/galaxy.py
+++ gcc2017/galaxy.py
@@ -1,8 +1,12 @@
 import galaxyxml.tool as gxt
 import galaxyxml.tool.parameters as gxtp
 from galaxyxml.tool.import_xml import GalaxyXmlParser
+from lxml import etree
+
+def tostring(galaxyxml_object):
+    return etree.tostring(galaxyxml_object.node)
 
 def export_parameter(parameter):
-    param = gxtp.DataParam(name, label=parameter.description,
+    param = gxtp.DataParam(parameter.name, label=parameter.description,
                            help=parameter.help)
     return param
