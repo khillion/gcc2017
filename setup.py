032e1b21e0c222c02ae7a6c40379856132d85ee0
Hervé  MENAGER <herve.menager@pasteur.fr>
2017-06-26 19:19:56 +0200

    add minimal setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='gcc2017',
+      version='0.0',
+      description='GCC2017 Tool Overlord',
+      url='http://github.com/khillion/gcc2017.git',
+      author='KHH, IK, HM',
+      license='MIT',
+      packages=['gcc2017'],
+      zip_safe=False)
